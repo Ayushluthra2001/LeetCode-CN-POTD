ffa9bd350e05ab7b3124ef9993c15d50e161df1d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-07-11 09:50:27 +0530

    Create 242. 有效的字母异位词.cpp

---
+++ 242. 有效的字母异位词.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.length() != t.length()) return false;
+        vector<int>freq(26,0);
+
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]-'a'] ++;
+        }
+        for(int i=0;i<t.length();i++){
+          
+            freq[t[i]-'a']--;
+        }
+
+        for(int i=0; i<26; i++){
+           // cout<<freq[i]<<" ";
+            if(freq[i] != 0 ) return false;
+        }
+        //cout<<endl;
+        return true;
+    }
+};
